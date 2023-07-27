7185acad850214ecde1d980fa7eeb9af60b8f304
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2023-07-27 13:19:31 +0200

    one style.css file, redirect for heroku, add favicon, add apple-touch-icon, add robots.txt, add better titles and descriptions, remove welcome from header

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, request
 from views import site
 import os
 
@@ -7,6 +7,12 @@ app = Flask(__name__)
 app.secret_key = 'secret'
 
 app.register_blueprint(site, url_prefix="")
+
+@app.before_request
+def redirect_to_www_and_https():
+    # Redirect non-www requests to www version (Heroku only)
+    if 'DYNO' in os.environ and not request.url.startswith('https://www.'):
+        return redirect(request.url.replace('https://', 'https://www.'), code=301)
 
 if __name__ == "__main__":
     app.run()
