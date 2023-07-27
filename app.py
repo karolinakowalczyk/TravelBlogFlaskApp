aa4330dc48b7fb96950dfd47e42705808df4f127
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2023-07-27 13:30:04 +0200

    remove redirect for heroku

--- app.py
+++ app.py
@@ -8,11 +8,5 @@ app.secret_key = 'secret'
 
 app.register_blueprint(site, url_prefix="")
 
-@app.before_request
-def redirect_to_www_and_https():
-    # Redirect non-www requests to www version (Heroku only)
-    if 'DYNO' in os.environ and not request.url.startswith('https://www.'):
-        return redirect(request.url.replace('https://', 'https://www.'), code=301)
-
 if __name__ == "__main__":
     app.run()
