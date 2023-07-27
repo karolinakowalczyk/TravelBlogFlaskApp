1fee9ee72c4c456d50f4b12e81dc7a6778330775
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2023-07-27 19:13:43 +0200

    redirect non-www urls to www deleted

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request
+from flask import Flask
 from views import site
 from flask_talisman import Talisman
 from flask_compress import Compress
@@ -12,14 +12,6 @@ app.register_blueprint(site, url_prefix="")
 Talisman(app, content_security_policy=None)
 
 Compress(app)
-
-canonical_domain = "travel-blog-flask-app-9b69d584e4f3.herokuapp.com"
-
-@app.before_request
-def redirect_heroku_domain():
-    if request.host != canonical_domain:
-        new_url = request.url.replace(request.host, canonical_domain, 1)
-        return redirect(new_url, code=301)
 
 if __name__ == "__main__":
     app.run(debug=False)
