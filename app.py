abf22c7bb4ca02cbaccc31a795050939121bbd44
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2023-07-27 18:41:58 +0200

    redirect non-www urls to www added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, request
 from views import site
 from flask_talisman import Talisman
 from flask_compress import Compress
@@ -12,6 +12,11 @@ app.register_blueprint(site, url_prefix="")
 Talisman(app, content_security_policy=None)
 
 Compress(app)
+
+def redirect_non_www():
+    if request.headers.get('Host') == 'www.travel-blog-flask-app-9b69d584e4f3.herokuapp.com':
+        new_url = request.url.replace('www.', '', 1)
+        return redirect(new_url, code=301)
 
 if __name__ == "__main__":
     app.run(debug=False)
