2605c99883737222192012d3550cdc13d901e60a
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2023-07-27 18:08:20 +0200

    redirect non-www urls to www

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, redirect, request
 from views import site
 from flask_talisman import Talisman
 from flask_compress import Compress
+from urllib.parse import urlparse, urlunparse
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
@@ -12,6 +13,15 @@ app.register_blueprint(site, url_prefix="")
 Talisman(app, content_security_policy=None)
 
 Compress(app)
+
+@app.before_request
+def redirect_nonwww():
+    """Redirect non-www requests to www."""
+    urlparts = urlparse(request.url)
+    if urlparts.netloc == 'travel-blog-flask-app-5d928a87ede1.herokuapp.com':
+        urlparts_list = list(urlparts)
+        urlparts_list[1] = 'www.travel-blog-flask-app-5d928a87ede1.herokuapp.com'
+        return redirect(urlunparse(urlparts_list), code=301)
 
 if __name__ == "__main__":
     app.run(debug=False)
