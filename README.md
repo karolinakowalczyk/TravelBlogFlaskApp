08c2bd5804f80a4be20e5e73292ed22c094cdcfa
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2023-07-27 17:43:40 +0200

    add social media to footer - github, add content, talisman to redirection, compress for gzip compression

---
+++ README.md
@@ -0,0 +1,4 @@
+# Travel Blog - 360 Travel Inspirations
+
+Application written in Flask.
+[See website](https://travel-blog-flask-app-5d928a87ede1.herokuapp.com/).
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, request
+from flask import Flask
 from views import site
-import os
+from flask_talisman import Talisman
+from flask_compress import Compress
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
@@ -8,5 +9,9 @@ app.secret_key = 'secret'
 
 app.register_blueprint(site, url_prefix="")
 
+Talisman(app, content_security_policy=None)
+
+Compress(app)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False)
