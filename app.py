4c7bbeababf8c4b9b5b83672c3b7ef4e5493b749
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2023-07-27 13:56:32 +0200

    update robots.txt and add static to app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, request
 from views import site
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 app.secret_key = 'secret'
 
